dc9b8e9a3718f69da373995cf18d26c74286688a
chhayatank <109730641+chhayatank@users.noreply.github.com>
2022-11-01 17:06:29 +0000

    Update unicorn.js

--- unicorn.js
+++ unicorn.js
@@ -1,1 +1,13 @@
-//this is a test
+
+function whatsYourName () {
+
+return prompt (“Hello! What is your name?”)
+
+
+}
+
+
+let whatsYourName = userName ();
+console.log(“Where’s your favourite place, ”, whatsYourName + “?”) 
+
+//ask ellie - how do i put the console into the pop up? 
