f12d7f1c80968f83d7ace876edf3225416009d12
chhayatank <109730641+chhayatank@users.noreply.github.com>
2022-11-06 17:34:49 +0000

    Update and rename unicorn.js to unicornscotland.js

---
+++ unicornscotland.js
@@ -0,0 +1,9 @@
+alert('Hello!')
+let userAnswer = prompt(' Have you been to Scotland before? ( y / n)')
+
+if(userAnswer === 'y'){
+    alert('That is great!')
+    console.log("User has been to Scotland before")
+}
+else{
+    alert("You should visit Scotland!")
